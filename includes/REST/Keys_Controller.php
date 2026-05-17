98cb09ba21b3befa5f1dead333cd9b3088dd3d32
pythongeek <tsnion@gmail.com>
2026-05-18 00:21:14 +0600

    Delete includes directory

--- includes/REST/Keys_Controller.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace AutoNode\REST;
-defined( 'ABSPATH' ) || exit;
-// Keys_Controller is defined in Webhook_Controller.php â€” loaded by autonode_load_files()
--- includes/REST/System_Controller.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace AutoNode\REST;
-defined( 'ABSPATH' ) || exit;
-// System_Controller is defined in Webhook_Controller.php â€” loaded by autonode_load_files()
